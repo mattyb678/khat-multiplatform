751dc7f56675b41880558a0ed3c0ed48c23f2e52
Matt Berteaux <matt.berteaux@gmail.com>
2018-11-11 09:39:07 -0700

    Oops, remove copyright

--- android/src/androidTest/java/com/meetup/kotlin/utah/ExampleInstrumentedTest.kt
+++ android/src/androidTest/java/com/meetup/kotlin/utah/ExampleInstrumentedTest.kt
@@ -1,23 +1,3 @@
-/*
- * ************************************************************************
- *  * ADOBE CONFIDENTIAL
- *  * ___________________
- *  *
- *  *  Copyright 2018 Adobe Systems Incorporated
- *  *  All Rights Reserved.
- *  *
- *  * NOTICE:  All information contained herein is, and remains
- *  * the property of Adobe Systems Incorporated and its suppliers,
- *  * if any.  The intellectual and technical concepts contained
- *  * herein are proprietary to Adobe Systems Incorporated and its
- *  * suppliers and are protected by all applicable intellectual property
- *  * laws, including trade secret and copyright laws.
- *  * Dissemination of this information or reproduction of this material
- *  * is strictly forbidden unless prior written permission is obtained
- *  * from Adobe Systems Incorporated.
- *  *************************************************************************
- */
-
 package com.meetup.kotlin.utah
 
 import android.support.test.InstrumentationRegistry
@@ -39,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.meetup.kotlin.utah", appContext.packageName)
+        assertEquals("com.meetup.kotlin.com.meetup.kotlin.utah", appContext.packageName)
     }
 }
